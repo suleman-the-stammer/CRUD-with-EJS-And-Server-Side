37ba9861dbc665597f3345f3724ae1e3fa46eaa9
stammer1 <stammer786@gmail.com>
2024-11-20 14:38:32 -0800

    Adding Features And Finalizing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const userModel = require('./models/user');
 
 app.set("view engine" , "ejs");
 app.use(express.json());
@@ -12,5 +13,13 @@ app.get('/' , (req, res)=>{
 })
 app.get('/read' , (req, res)=>{
     res.render('read')
+})
+app.post('/create' , function(req , res){
+    let {name , email , image } = req.body;
+    let newUser = userModel.create({
+        name,
+        email,
+        image
+    });
 })
 app.listen(3000)
